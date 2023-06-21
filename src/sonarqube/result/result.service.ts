f4443bd95c507486ae34eba09502deb173a9e342
TranNgocVy <ngocvy04042001@gmail.com>
2023-06-21 20:40:35 +0700

    update getResultBySubmisisonId function

--- src/sonarqube/result/result.service.ts
+++ src/sonarqube/result/result.service.ts
@@ -57,8 +57,8 @@ export class ResultService {
               'bugs,vulnerabilities,code_smells,sqale_index,duplicated_lines_density,ncloc,coverage,reliability_rating,security_rating,sqale_rating,violations,blocker_violations,critical_violations,major_violations,minor_violations,info_violations',
             // from: ,
             // to: ,
-            // p: page,
-            // ps: pageSize,
+            p: page,
+            ps: pageSize,
           },
           timeout: 60000,
         })
@@ -68,12 +68,63 @@ export class ResultService {
     });
 
     if (data !== null) {
-      return {
-        paging: data.paging,
-        measures: data.measures,
-      };
+      const resultPageIndex = data.paging.pageIndex;
+      const resultPageSize = data.paging.pageSize;
+      const resultTotal = data.paging.total;
+
+      if (resultPageIndex * resultPageSize < resultTotal) {
+        console.log((resultTotal % resultPageSize) + 1);
+        const result = await firstValueFrom(
+          this.httpService
+            .get(`${url}/measures/search_history`, {
+              auth: auth,
+              params: {
+                component: project.key,
+                metrics:
+                  'bugs,vulnerabilities,code_smells,sqale_index,duplicated_lines_density,ncloc,coverage,reliability_rating,security_rating,sqale_rating,violations,blocker_violations,critical_violations,major_violations,minor_violations,info_violations',
+                // from: ,
+                // to: ,
+                p:
+                  Math.floor(resultTotal / resultPageSize) +
+                  (resultTotal % resultPageSize == 0 ? 0 : 1),
+                ps: resultPageSize,
+              },
+              timeout: 60000,
+            })
+            .pipe(),
+        ).catch((e) => {
+          throw e;
+        });
+
+        if (result.data !== null) {
+          return {
+            paging: result.data.paging,
+            measures: result.data.measures,
+          };
+        }
+      } else {
+        return {
+          paging: data.paging,
+          measures: data.measures,
+        };
+      }
     }
 
     return null;
+  }
+
+  async getOverviewResultsBySubmissionId(submissionId: string): Promise<any> {
+    return await this.getResultsBySubmissionId(submissionId, 1, 100)
+      .then((result) => {
+        const overview = {};
+        result.measures.forEach((metric) => {
+          overview[`${metric.metric}`] =
+            metric.history[metric.history.length - 1].value;
+        });
+        return overview;
+      })
+      .catch((e) => {
+        return null;
+      });
   }
 }
