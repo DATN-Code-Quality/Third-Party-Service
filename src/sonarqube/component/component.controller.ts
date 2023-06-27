5018468ce11aa3632ff2fb64e8181862c8e69b6c
TranNgocVy <ngocvy04042001@gmail.com>
2023-06-27 16:01:16 +0700

    add api get component

---
+++ src/sonarqube/component/component.controller.ts
@@ -0,0 +1,44 @@
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { Metadata } from '@grpc/grpc-js';
+import { ComponentService } from './component.service';
+import { ComponentRequest, ComponentResponse } from './interfaces/Component';
+
+@Controller('components')
+export class ComponentController {
+  constructor(private readonly componentService: ComponentService) {}
+
+  @GrpcMethod('GSonarqubeService', 'getComponentsBySubmissionId')
+  async getComponentsBySubmissionId(
+    data: ComponentRequest,
+    meta: Metadata,
+  ): Promise<ComponentResponse> {
+    var result: ComponentResponse;
+    await this.componentService
+      .getComponentsBySubmissionId(data.submissionId, data.page, data.pageSize)
+      .then((component) => {
+        if (component) {
+          result = {
+            data: component,
+            error: 0,
+            message: null,
+          };
+        } else {
+          result = {
+            message: 'No data Found',
+            data: null,
+            error: 1,
+          };
+        }
+      })
+      .catch((e) => {
+        result = {
+          message: e,
+          data: null,
+          error: 1,
+        };
+      });
+
+    return result;
+  }
+}
---
+++ src/sonarqube/component/component.service.ts
@@ -0,0 +1,66 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+import { ProjectService } from 'src/project/project.service';
+import { Component, Components } from '../Component/interfaces/Component';
+import { PROJECT_TYPE } from 'src/project/req/project-req.dto';
+
+@Injectable()
+export class ComponentService {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly projectService: ProjectService,
+  ) {}
+
+  async getComponentsBySubmissionId(
+    submissionId: string,
+    page: number,
+    pageSize: number,
+  ): Promise<Components> {
+    const project = await this.projectService.findProjectBySubmissionId(
+      submissionId,
+    );
+    if (project == null) {
+      return null;
+    }
+
+    let url = null;
+    let auth = null;
+
+    if (project.type === PROJECT_TYPE.C_CPP) {
+      url = process.env.SONARCLOUD_URL;
+    } else {
+      url = process.env.SONARQUBE_BASE_URL;
+      auth = {
+        username: process.env.SONARQUBE_USERNAME,
+        password: process.env.SONARQUBE_PASSWORD,
+      };
+    }
+
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get(`${url}/components/tree`, {
+          auth: auth,
+          params: {
+            component: project.key,
+            qualifiers: 'FIL',
+            p: page,
+            ps: pageSize,
+          },
+          timeout: 60000,
+        })
+        .pipe(),
+    ).catch((e) => {
+      throw e;
+    });
+
+    if (data !== null) {
+      return {
+        paging: data.paging,
+        components: data.components,
+      };
+    }
+
+    return null;
+  }
+}
